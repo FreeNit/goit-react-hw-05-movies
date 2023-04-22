9a026ea345d20cad6d93a02fa4627e245a391191
Dimon <bolilui.dmytro@gmail.com>
2023-04-22 18:29:34 +0300

    Add Query Movies

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,11 +1,55 @@
+import { useState, useEffect } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
+
+import fetchByQuery from 'services/fetchByQuery';
+
 const Movies = () => {
+  const [inputQuery, setInputQuery] = useState('');
+  const [movieCollection, setMovieCollection] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const queryValue = searchParams.get('query') ?? '';
+
+  useEffect(() => {
+    if (queryValue) {
+      console.log('NEW REQUEST');
+      const getMovieCollectionByQuery = async () => {
+        const {
+          data: { results },
+        } = await fetchByQuery(queryValue);
+        setMovieCollection(results);
+      };
+      getMovieCollectionByQuery();
+    }
+  }, [queryValue]);
+
+  const handleChange = e => {
+    const inputValue = e.target.value.toLowerCase();
+    setInputQuery(inputValue);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const requestQuery = inputQuery !== '' ? { query: inputQuery } : {};
+
+    setSearchParams(requestQuery);
+  };
+
   return (
     <>
-      <p>Movies Page</p>
-      <p>
-        '/movies' – компонент Movies, сторінка пошуку кінофільмів за ключовим
-        словом.
-      </p>
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <input type="text" value={inputQuery} onChange={handleChange} />
+        <button type="submit">Search</button>
+      </form>
+      {movieCollection && (
+        <div>
+          {movieCollection.map(movie => (
+            <li key={movie.id}>
+              <Link to={`/movies/${movie.id}`}>{movie.original_title}</Link>
+            </li>
+          ))}
+        </div>
+      )}
     </>
   );
 };
